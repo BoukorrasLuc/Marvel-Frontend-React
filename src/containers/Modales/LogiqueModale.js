dbc2b3bae4e729bd168ed1383fb70addbf8cea8b
unknown <l.boukorras@gmail.com>
2021-10-14 18:55:40 +0200

    creation of the function for the signUp modal

--- src/containers/Modales/LogiqueModale.js
+++ src/containers/Modales/LogiqueModale.js
@@ -2,15 +2,22 @@ import { useState } from "react";
 
 const LogiqueModale = () => {
   const [reveleModaleLogin, changeReveleModaleLogin] = useState(false);
+  const [reveleModaleSignUp, changeReveleModaleSignUp] = useState(false);
 
   function toggleModaleLogin() {
     changeReveleModaleLogin(!reveleModaleLogin);
+  }
+  function toggleModaleSignUp() {
+    changeReveleModaleSignUp(!reveleModaleSignUp);
   }
 
   return {
     reveleModaleLogin,
     toggleModaleLogin,
     changeReveleModaleLogin,
+    reveleModaleSignUp,
+    toggleModaleSignUp,
+    changeReveleModaleSignUp,
   };
 };
 
