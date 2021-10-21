b7db72f7020013f0bb9ec6eafeb80fdb86470c11
unknown <l.boukorras@gmail.com>
2021-10-21 18:12:29 +0200

    problem solved on the last display problem

--- src/components/ComicsWrapped.js
+++ src/components/ComicsWrapped.js
@@ -37,8 +37,6 @@ const ComicsWrapped = ({
         }
         let newDescription = removeHTMLTag(comics.description);
 
-        console.log(comics);
-
         return (
           <div key={comics._id}>
             {(comics.thumbnail.path === noImage &&
